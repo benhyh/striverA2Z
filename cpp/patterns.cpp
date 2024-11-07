a4ccd69f905b619acc6b41beb983e595bd96dbe5
benhuynh003 <155077782+benhuynh003@users.noreply.github.com>
2024-11-07 00:32:07 -0800

    pattern 21

--- cpp/patterns.cpp
+++ cpp/patterns.cpp
@@ -331,7 +331,26 @@ void pattern20 (int n) {
         cout << endl;
     }
 }
-void pattern21 (int n) {}
+void pattern21 (int n) {
+    for (int i = 0; i < n; i++) {
+    if (i == 0 || i == n-1) {
+        for (int j = 0; j < n; j++) {
+            cout << "*";
+        }
+    } else {
+        for (int j = 0; j < 1; j++) {
+            cout << "*";
+        }
+        for (int j = 0; j < n; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < 1; j++) {
+            cout << "*";
+        }
+    }
+    cout << endl;
+    }
+}
 void pattern22 (int n) {}
 
 int main() {
