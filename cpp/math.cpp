8ff118e50509fe16ed95896177a6dd4da868e27d
benhuynh003 <155077782+benhuynh003@users.noreply.github.com>
2024-11-15 13:09:15 -0800

    palindrome

--- cpp/math.cpp
+++ cpp/math.cpp
@@ -25,8 +25,39 @@ int reverse(int x) {
         value /= 10;
         if (reversed > INT_MAX / 10 || (reversed == INT_MAX / 10 && digit > 7)) return 0;
         if (reversed < INT_MIN / 10 || (reversed == INT_MIN / 10 && digit < -8)) return 0;
-        reversed = reversed * 10 + digit;
+        reversed = reversed * 10 + digit; 
     }
     return reversed;
 }
 
+// LC palindrome
+bool isPalindrome(int x) {
+    string converted = std::to_string(x);
+    string compare = std::to_string(x);
+
+    int l = 0;
+    int r = converted.length() - 1;
+
+    while (l < r) {
+        swap(converted[l], converted[r]);
+            
+        l++;
+
+        r--;
+    }
+
+    return (compare == converted);
+}
+
+bool isPaplindromeInt(int x) {
+    int dupe = x;
+    int reversedValue = 0;
+
+    while (0 < dupe) { 
+        int digit = dupe % 10;
+        dupe /= 10;
+        reversedValue = reversedValue * 10 + digit;
+    }
+
+    return (x == reversedValue);
+}
